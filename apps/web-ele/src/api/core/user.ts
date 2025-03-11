fd3ae1654ac10294c5a6ad5b8cf8e1bd72c79f52
ping-an-na <zhangfenghui11@163.com>
2025-03-11 14:10:15 +0800

    baidu api

--- apps/web-ele/src/api/core/user.ts
+++ apps/web-ele/src/api/core/user.ts
@@ -1,10 +1,23 @@
-import type { UserInfo } from '@vben/types';
-
-import { requestClient } from '#/api/request';
-
 /**
  * 获取用户信息
  */
 export async function getUserInfoApi() {
-  return requestClient.get<UserInfo>('/user/info');
+  // console.log(requestClient.get<UserInfo>('/user/info'));
+  // return requestClient.get<UserInfo>('/user/info');
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('First');
+      resolve({aaa: 'aaa'});
+    }, 1000);
+  });
+  // return
+}
+
+/**
+ * 百度
+ */
+export async function baidu(data) {
+  return requestClientB.get<UserInfo>(`/api?${data}`);
+  // const queryString = `https://fanyi-api.baidu.com/api/trans/vip/translate?${objectToQueryString(apiData)}`;
 }
